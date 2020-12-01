4128b06f5301d735be8a799ba40a7862ed2c4a91
fiona <fionaniwe@gmail.com>
2020-12-01 07:09:35 +0200

    views.py file is created

--- app/models.py
+++ app/models.py
@@ -26,6 +26,7 @@ class User(UserMixin,db.Model):
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
 
 
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
